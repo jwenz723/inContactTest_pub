ab590a162cf588f9b395372f881bc7b43c3f5b11
jwenz723 <jeff.wenzbauer@incontact.com>
2017-03-08 15:59:36 -0700

    modified script to post new metric to pulse using api

--- get-counters.ps1
+++ get-counters.ps1
@@ -1,7 +1,23 @@
-    
 [string[]]$fcounters = $param.counters
 [string[]]$fmetrics = $param.metrics
 [float[]]$fmultipliers = $param.multipliers
+
+$JSON = '
+{
+   "name": "some_metric_id",
+   "description": "Windows Performance Counter",
+   "displayName": "Metric Name",
+   "displayNameShort": "metname",
+   "unit": "number",
+   "defaultAggregate": "avg"
+}'
+
+$pair = "jwenz723@gmail.com:59247fb2-727c-45e2-a2a2-e35f66f80b3a"
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
+$base64 = [System.Convert]::ToBase64String($bytes)
+$basicAuthValue = "Basic $base64"
+
+Invoke-WebRequest -Uri https://api.truesight.bmc.com/v1/metrics -Method POST -Body $JSON -Headers @{Authorization = "$basicAuthValue"} -ContentType 'application/json'
 
 $hostname = Get-Content Env:\COMPUTERNAME
 
